b6be4eb09ca54419fd84a8496deb9a0a43204c4b
CANE4KA <145180300+CANE4KA@users.noreply.github.com>
2024-10-24 21:14:31 +0500

    Bug fix

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -5,10 +5,10 @@ import styles from './NotFound.module.scss'
 export const NotFound = () => {
 	return (
 		<div className={styles.container}>
-			<h2 className={styles.oops}>Oops! Page not found</h2>
+			<h2 className={styles.oops}>Уупс! Страница не найдена</h2>
 			<h1 className={styles.code}>404</h1>
-			<p>We can't find the page you're looking for.</p>
-			<Link to='/weather-app'>Go back home</Link>
+			<p>Мы не можем найти страницу, которую вы ищете.</p>
+			<Link to='/weather-app'>Вернуться домой</Link>
 		</div>
 	)
 }
